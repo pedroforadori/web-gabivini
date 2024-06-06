e38d22e895772bedb496704ac12febe9b88992ac
pedro-foradori <analistamobile.logtech@trouw.com.br>
2024-06-06 18:30:18 -0300

    feat: texto home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen w-full justify-start flex flex-col items-center bg-no-repeat max-sm:bg-left max-sm:mt-[-3rem]">
+    <div className="bg-white min-h-screen w-full justify-center flex flex-col items-center bg-no-repeat max-sm:bg-left max-sm:mt-[-3rem]">
       {/* <div className="max-sm:flex max-sm:items-start max-sm:w-full max-sm:p-2">
         <Menu />
       </div> */}
-      <div className="flex flex-col items-center mt-[-3rem] max-sm:mt-10">
-        {/* <div className="text-right z-10 relative top-[-2rem] left-24 tracking-wider max-sm:right-0 max-sm:left-0">
-          <div className="text-marinho text-[10rem] mt-10 mb-9 max-sm:text-4xl font-modernSymphony">
-            Gabriela &
-          </div>
-          <div className="text-marinho text-[10rem] mb-9 max-sm:text-4xl font-light mt-[-10rem] font-modernSymphony">
-            Vinicius
-          </div>
-        </div> */}
-        <div className="text-right max-sm:right-0 max-sm:left-0 z-10">
+      <div className="w-1/2 flex flex-col items-center mt-[-3rem] max-sm:mt-10 max-sm:w-full">
+        <div className="text-center max-sm:right-0 max-sm:left-0 z-10">
           <div className="text-marinho text-[10rem] max-sm:text-8xl max-sm:mb-4 font-modernSymphony">
             Gabriela & Vinicius
+          </div>
+          <div className="text-marinho text-xl font-light mb-4 text-justify max-sm:mb-4 max-sm:mx-4">
+            "Deus mudou o teu caminho até juntares com o meu e guardou a tua vida separando-a para mim. Para onde fores, irei; onde tu repousares, repousarei. Teu Deus será o meu Deus. Teu caminho o meu será." (Rute 1:16-18)
           </div>
         </div>
         <Image
           className=" max-sm:mt-0"
           src={require("../../public/imageMain.png")}
           alt="imagem principal"
-          width={400}
+          width={300}
           quality={100}
         />
       </div>
