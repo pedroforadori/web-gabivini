3de6e5c9585cbe6832b4f7346eda86636af68952
pedro-foradori <analistamobile.logtech@trouw.com.br>
2024-06-06 18:36:39 -0300

    feat: texto home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,17 @@ export default function Home() {
       {/* <div className="max-sm:flex max-sm:items-start max-sm:w-full max-sm:p-2">
         <Menu />
       </div> */}
-      <div className="w-1/2 flex flex-col items-center mt-[-3rem] max-sm:mt-10 max-sm:w-full">
+      <div className="w-2/5 flex flex-col items-center mt-[-3rem] max-sm:mt-10 max-sm:w-full">
         <div className="text-center max-sm:right-0 max-sm:left-0 z-10">
           <div className="text-marinho text-[10rem] max-sm:text-8xl max-sm:mb-4 font-modernSymphony">
             Gabriela & Vinicius
           </div>
           <div className="text-marinho text-xl font-light mb-4 text-justify max-sm:mb-4 max-sm:mx-4">
-            "Deus mudou o teu caminho até juntares com o meu e guardou a tua vida separando-a para mim. Para onde fores, irei; onde tu repousares, repousarei. Teu Deus será o meu Deus. Teu caminho o meu será." (Rute 1:16-18)
+            {`"Deus mudou o teu caminho até juntares com o meu e guardou a tua vida separando-a para mim. Para onde fores, irei; onde tu repousares, repousarei. Teu Deus será o meu Deus. Teu caminho o meu será." (Rute 1:16-18)`}
           </div>
         </div>
         <Image
-          className=" max-sm:mt-0"
+          className="max-sm:mt-0"
           src={require("../../public/imageMain.png")}
           alt="imagem principal"
           width={300}
