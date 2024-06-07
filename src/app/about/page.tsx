a101ce86dbd22c49961749bd99aba28d2e8c285f
pedro-foradori <analistamobile.logtech@trouw.com.br>
2024-06-07 16:34:37 -0300

    feat: conteudo local da festa atualizado

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,19 +10,23 @@ export default function About() {
             </h1>
             <div className="flex justify-end items-center h-[80vh] max-sm:flex max-sm:flex-col-reverse max-sm:justify-around">
                 <div className="relative left-40 max-sm:left-0 max-sm:mx-4">
-                    <p className="text-marinho font-light">ENDEREÇO: AV. MAL. RONDON, 840 - CENTRO, OSASCO - SP, 06093-010</p>
-                    <p className="text-marinho font-light">HORÁRIO: 19:30</p>
-                    <p className="text-marinho font-light">TRAJE: SOCIAL COMPLETO</p>
-                    <div className="text-marinho text-xs font-light">
+                    <p className="text-marinho font-light"><span className="font-normal">ENDEREÇO: </span> AV. MAL. RONDON, 840 - CENTRO, OSASCO - SP, 06093-010</p>
+                    <p className="text-marinho font-light"><span className="font-normal">HORÁRIO: </span>19:30</p>
+                    <p className="text-marinho font-light"><span className="font-normal">TRAJE: </span> SOCIAL COMPLETO</p>
+                    {/* <div className="text-marinho text-xs font-light">
                         <Countdown />
-                    </div>
+                    </div> */}
                     
-                    <Link href={"https://waze.com/ul/h6gydpku04"}>
+                    {/* <Link href={"https://waze.com/ul/h6gydpku04"}>
                         <Image src={require("../../../public/waze.png")} width={50} alt="waze logo" />
-                    </Link>
+                    </Link> */}
                         
                 </div>
-                <Image src={require("../../../public/trem.jpeg")} width={1100} alt="trem logo" />
+                <div className="flex flex-col items-center">
+                    <Image src={require("../../../public/trem.jpeg")} width={800} alt="trem logo" />
+                    <p className="text-marinho font-light">14.12.2024</p>
+                </div>
+                
             </div>
         </div>
     )
