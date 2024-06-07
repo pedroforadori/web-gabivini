2a5fbd4daa6ab2f35a2a4cd186aa5d170c9ebcce
pedro-foradori <analistamobile.logtech@trouw.com.br>
2024-06-07 14:21:30 -0300

    feat: inicio style local da festa

--- src/app/local/page.tsx
+++ src/app/local/page.tsx
@@ -1,9 +1,12 @@
 export default function Local() {
     return (
-        <div>
-            <h1 className="text-9xl text-terracota text-bold max-sm:text-8xl max-sm:text-wedding-500 font-modernSymphony mt-12">
-                Confirme sua presença aqui!
+        <div className="bg-white h-screen w-full items-center flex-col">
+            <h1 className="text-offwhite text-8xl bg-terracota text-center max-sm:mt-5 font-modernSymphony">
+                Local da Festa
             </h1>
+            <div className="flex justify-center items-center">
+
+            </div>
         </div>
     )
 }
