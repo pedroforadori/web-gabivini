c31aa66cceede89f62f0052e7005458427bcea8b
pedro-foradori <analistamobile.logtech@trouw.com.br>
2024-06-07 17:51:52 -0300

    fix: layout guest mobile

--- src/app/guest/page.tsx
+++ src/app/guest/page.tsx
@@ -36,12 +36,12 @@ async function listGuest() {
 }
 
   return (
-    <div className="grid grid-cols-2 w-1/2 mx-auto max-sm:p-2 bg-marinho p-10 mt-10 rounded-lg bg-opacity-80 
-                    max-sm:flex max-sm:flex-col max-sm:w-full max-sm:mt-0 max-sm:h-screen max-sm:rounded-none">
+    <div className="grid grid-cols-2 w-1/2 mx-auto max-sm:p-2 bg-marinho p-10 mt-10 rounded-lg bg-opacity-80
+                    max-sm:flex max-sm:flex-col max-sm:w-full max-sm:mt-10 max-sm:h-screen max-sm:rounded-none max-sm:bg-white max-sm:text-marinho">
         <div className="text-wedding-500 text-2xl font-bold max-sm:text-xl">
             Numero de convidados confirmados: {guest?.length}
         </div>
-        <div className="text-white text-2xl flex flex-col items-center w-full font-bold max-sm:text-xl max-sm:mt-4">
+        <div className="text-white text-2xl flex flex-col items-center w-full font-bold max-sm:text-xl max-sm:mt-4 max-sm:text-marinho">
            <ol className="list-decimal">
                 {guest?.map(item => (
                     <li key={item.phone}> {item.name} </li>
