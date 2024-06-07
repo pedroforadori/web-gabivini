8e79ada1c45f80c1d73b29727ba446c64802322b
pedro-foradori <analistamobile.logtech@trouw.com.br>
2024-06-07 13:29:43 -0300

    feat: Qr code

--- src/components/CardGift.tsx
+++ src/components/CardGift.tsx
@@ -1,7 +1,6 @@
 "use client";
 import listGift from "@/data/listGift";
 import Image from "next/image";
-import Link from "next/link";
 import {
   Modal,
   ModalContent,
@@ -25,6 +24,12 @@ export default function CardGift() {
   const [emailSucess, setEmailSucess] = useState<string | any>("");
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const base = "http://localhost:3000";
+
+  const links = "gabilinsda@gmail.com";
+  const copyQr = (e: any) => {
+    navigator.clipboard.writeText(links)
+  }
 
   const backdrops = ["blur"];
 
@@ -80,9 +85,44 @@ export default function CardGift() {
       );
   }
 
+  // function copyQr() {
+  //   var copyText = "gabilinsda@gmail.com"
+  //   const links = copyText
+  //   const copylink = (e: any) => {
+  //     navigator.clipboard.writeText(links)
+  //   }
+  // }
+
   return (
     <>
-      <div className="text-telha h-auto flex flex-wrap gap-4 my-4 w-3/5 justify-center items-center max-lg:w-1/2">
+      <div className="text-telha h-auto flex flex-wrap gap-4 my-4 w-3/5 justify-center items-center max-lg:w-1/2 max-sm:w-11/12">
+        <div className="flex flex-row items-center max-sm:flex-col">
+          <div className="font-light text-justify mx-10 text-sm max-sm:mx-0 max-sm:w-full">
+            <p className="">{`Tudo isso um dia foi um sonho. Sonho construído não somente pelas nossas mãos, mas por todas as mãos que passaram e abençoaram o nosso caminho. Obrigada por fazer parte da nossa história!`}</p>
+            <p className="">{`Aos que desejam nos presentear, deixamos cotas da nossa lua de mel na Disney e Miami como opção!`}</p>
+            {/* <p>{`Ainda que eu fale as línguas dos homens e dos anjos, se não tiver amor, serei como o sino que ressoa ou como o prato que retine. Ainda que eu tenha o dom de profecia, saiba todos os mistérios e todo o conhecimento e tenha uma fé capaz de mover montanhas, se não tiver amor, nada serei. Ainda que eu dê aos pobres tudo o que possuo e entregue o meu corpo para ser queimado, se não tiver amor, nada disso me valerá. (Coríntios 13:1-3)`}</p> */}
+            <p className="">{`Caso prefiram realizar o pix diretamente, pedimos a gentileza que utilizem o QR code disponibilizado ao lado.`}</p>
+          </div>
+          <div className="mx-10 max-sm:flex-row max-sm:mx-0 max-sm:justify-center max-sm:items-center">
+            <Image
+              className=""
+              src={require("../../public/qrcode.png")}
+              alt="imagem principal"
+              width={150}
+              quality={100}
+            />
+            <button
+              // onPress={() => handleOpen(b, item.buy)}
+              onClick={copyQr}
+              className="bg-terracota max-sm:px-4 max-sm:py-3"
+            >
+              <p className="text-offwhite font-light">Copiar QR Code</p>
+            </button>
+          </div>
+
+        </div>
+
+
         {listGift.map((item) => (
           <div
             key={item.id}
@@ -151,8 +191,8 @@ export default function CardGift() {
                     {message?.length == ""
                       ? ""
                       : message?.length > 20
-                      ? ""
-                      : `${message?.length} / 20 caracteres restantes`}
+                        ? ""
+                        : `${message?.length} / 20 caracteres restantes`}
                   </p>
                 </form>
               </ModalBody>
