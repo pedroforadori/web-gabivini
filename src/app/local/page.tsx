7a61021e23ae718df45026c821b8fc4285d985fe
pedro-foradori <analistamobile.logtech@trouw.com.br>
2024-06-07 13:48:33 -0300

    feat: new page locals

---
+++ src/app/local/page.tsx
@@ -0,0 +1,9 @@
+export default function Local() {
+    return (
+        <div>
+            <h1 className="text-9xl text-terracota text-bold max-sm:text-8xl max-sm:text-wedding-500 font-modernSymphony mt-12">
+                Confirme sua presença aqui!
+            </h1>
+        </div>
+    )
+}
