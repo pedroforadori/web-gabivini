9887132649280cb531677d4e4fbe456891512dac
pedro-foradori <analistamobile.logtech@trouw.com.br>
2024-06-14 13:03:15 -0300

    feat: nova foto + countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,20 @@ export default function Home() {
       {/* <div className="max-sm:flex max-sm:items-start max-sm:w-full max-sm:p-2">
         <Menu />
       </div> */}
-      <div className="w-2/5 flex flex-col items-center  max-sm:w-full">
+      <div className="w-7/12 flex flex-col items-center  max-sm:w-full">
         <div className="text-center max-sm:right-0 max-sm:left-0 z-10">
-          <div className="text-marinho text-[10rem] max-lg:text-8xl max-xl:text-8xl max-2xl:text-8xl max-2xl:mb-4 max-lg:mb-4 max-sm:text-8xl max-sm:mb-4 font-modernSymphony">
+          <div className="text-marinho text-[10rem] max-lg:text-8xl max-xl:text-9xl max-2xl:text-8xl max-2xl:mb-4 max-lg:mb-4 max-sm:text-8xl max-sm:mb-4 font-modernSymphony">
             Gabriela & Vinicius
           </div>
-          <div className="text-marinho text-[10px] font-light mb-4 text-justify max-sm:my-8 max-sm:mx-12">
+          <div className="text-marinho text-xs font-light mb-4 text-justify max-sm:my-8 max-sm:mx-12">
             {`"Deus mudou o teu caminho até juntares com o meu e guardou a tua vida separando-a para mim. Para onde fores, irei; onde tu repousares, repousarei. Teu Deus será o meu Deus. Teu caminho o meu será." (Rute 1:16-18)`}
           </div>
         </div>
         <Image
           className="max-sm:mt-0"
-          src={require("../../public/imageMain.png")}
+          src={require("../../public/imageMain.jpg")}
           alt="imagem principal"
-          width={300}
+          width={1100}
           quality={100}
         />
       </div>
--- src/components/CountDownTimer.tsx
+++ src/components/CountDownTimer.tsx
@@ -9,27 +9,28 @@ export default function Countdown() {
     const difference = targetDate.getTime() - now.getTime();
 
     let timeLeft = {
-      months: 0,
+//      months: 0,
       days: 0,
       hours: 0,
       minutes: 0,
-      seconds: 0,
+      // seconds: 0,
     };
 
     if (difference > 0) {
       timeLeft = {
-        months: Math.floor(
-          (difference % (1000 * 60 * 60 * 24 * 365)) /
-            (1000 * 60 * 60 * 24 * 30)
-        ),
-        days: Math.floor(
-          (difference % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
-        ),
+        // months: Math.floor(
+        //   (difference % (1000 * 60 * 60 * 24 * 365)) /
+        //     (1000 * 60 * 60 * 24 * 30)
+        // ),
+        // days: Math.floor(
+        //   (difference % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+        // ),
         hours: Math.floor(
           (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
         ),
         minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
-        seconds: Math.floor((difference % (1000 * 60)) / 1000),
+        // seconds: Math.floor((difference % (1000 * 60)) / 1000),
+        days: Math.floor(difference / (1000 * 60 * 60 * 24))
       };
     }
 
@@ -49,7 +50,8 @@ export default function Countdown() {
   return (
     <div className="flex items-center flex-col">
       <p className="text-4xl mt-10">
-        {timeLeft.months} M | {timeLeft.days} D{" "}| {timeLeft.hours <= 9 ? `0${timeLeft.hours}` : timeLeft.hours} H
+        {/* {timeLeft.months} M | {timeLeft.days} D{" "}| {timeLeft.hours <= 9 ? `0${timeLeft.hours}` : timeLeft.hours} H */}
+       {timeLeft.days} D{" "}| {timeLeft.hours <= 9 ? `0${timeLeft.hours}` : timeLeft.hours} H{" "}| {timeLeft.minutes <= 9 ? `0${timeLeft.minutes}` : timeLeft.minutes} M
       </p>
       {/* <p className="text-6xl font-light max-sm:text-4xl">
         {timeLeft.hours <= 9 ? `0${timeLeft.hours}` : timeLeft.hours}:{timeLeft.minutes <= 9 ? `0${timeLeft.minutes}` : timeLeft.minutes}:{timeLeft.seconds <= 9 ? `0${timeLeft.seconds}` : timeLeft.seconds}{" "}
